f01211a947e335b3d7b630af57b5931e58feff05
Kumbirai Mubaiwa <mubaiwakumbirai24@gmail.com>
2024-05-04 22:28:19 +0200

    Added Components for popular for Women

--- src/components/popular/popular.jsx
+++ src/components/popular/popular.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import "./popular.css";
+import React from "react";
+import "./Popular.css";
 import data_product from "../Assets/data";
 
-const popular = () => {
+const Popular = () => {
   return (
-    <div className='popular'>
+    <div className="popular">
       <h1>POPULAR IN WOMEN</h1>
-      <hr/>
-      <div className='popular-item'>
-         {data_product.map((item, i) => {
-            return <item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-         })}
+      <hr />
+      <div className="popular-item">
+        {data_product.map((item) => (
+          <div key={item.id}>
+            <img src={item.image} alt={item.name} />
+            <h2>{item.name}</h2>
+            <p>New Price: {item.new_price}</p>
+            <p>Old Price: {item.old_price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default popular;
+export default Popular;
