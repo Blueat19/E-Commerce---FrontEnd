cc3297acfb45f9a0d265ca604a38b8e926fafddf
Kumbirai Mubaiwa <mubaiwakumbirai24@gmail.com>
2024-05-22 22:16:31 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,25 +8,49 @@ const CustomNavbar = () => {
 
   const [menu,setMenu] = useState("shop");
 
-  return(
-    <div className='navbar'>
-      <div className='nav-logo'>
-      <img src={logo} alt=''/>
-      <p>SHOPPER</p>
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
       </div>
-      
-      <ul className='nav-menu'>
-        <li onClick={() => setMenu("shop")}><Link style={{textDecoration:"none" }} to="/">Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={() => setMenu("mens")}><Link style={{textDecoration:"none" }} to="/">Men</Link>{menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={() => setMenu("womens")}><Link style={{textDecoration:"none" }} to="/">Women</Link>{menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={() => setMenu("kids")}><Link style={{textDecoration:"none" }} to="/">Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-      </ul>
-      <div className='nav-login-cart'>
-       <Link style={{textDecoration:"none" }} to="/login"> <button>Login</button></Link>
-        <Link style={{textDecoration:"none" }} to="/cart"><img src={cart_icon} alt=""/></Link>
-        <div className='nav-cart-count'>0</div>
-      </div>    
 
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          <Link style={{ textDecoration: "none" }} to="/womens">
+            Women
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link style={{ textDecoration: "none" }} to="/login">
+          {" "}
+          <button>Login</button>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   );
 }
