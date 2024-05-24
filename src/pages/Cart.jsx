19c9cafd92a1b579a0c0a4042b2ba213c91ff8ef
Kumbirai Mubaiwa <mubaiwakumbirai24@gmail.com>
2024-05-24 23:37:22 +0200

    created cart.jsx and cart.css

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import "./css/Cart.css";
 
 const Cart = () => {
   return (
-    <div>
-      
+    <div className="cart-page">
+      <h1>Cart</h1>
+      <ul className="cart-items">{/* cart items will be rendered here */}</ul>
+      <div className="cart-total">
+        <p>Total: $0</p>
+      </div>
+      <div className="cart-actions">
+        <button>Checkout</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart
+
